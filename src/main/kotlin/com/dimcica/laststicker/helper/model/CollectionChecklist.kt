2c43d1c415dbcfe30721e1717a97065f591df7dc
Tudor Dimcica <tudor.dimcica@gmail.com>
2022-11-14 02:26:32 +0200

    Enhanced CollectionChecklist.

--- src/main/kotlin/com/dimcica/laststicker/helper/model/CollectionChecklist.kt
+++ src/main/kotlin/com/dimcica/laststicker/helper/model/CollectionChecklist.kt
@@ -6,5 +6,14 @@ import org.springframework.data.mongodb.core.mapping.Document
 @Document("checklists")
 data class CollectionChecklist(
     @Id val collectionName: String,
-    val checklist: String
+    val collectionLink: String,
+    val checklist: Map<String, CardDetail>
+)
+
+data class CardDetail(
+    val title: String,
+    val section: String,
+    val type: String,
+    val need: Int,
+    val offer: Int
 )
